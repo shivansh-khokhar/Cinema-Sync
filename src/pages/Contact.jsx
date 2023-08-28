b154d342f3b1cecafda02810fbe29d4e2357e781
Shivansh <shivakhokhar4600@gmail.com>
2023-08-28 23:16:36 +0530

    added pages via routing

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Get in touch with us through our contact information.</p>
+    </div>
+  );
+}
+
+export default Contact;
