a12a969675ff80ad38e7f6503a0431f90d376bc2
Shivansh Khokhar <shivakhokhar4600@gmail.com>
2023-11-21 12:27:10 +0530

    deployed to github pages and used hashrouter instead of browser router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MainLayout from "./components/mainLayout";
 import Starred from "./pages/Starred";
@@ -11,7 +11,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalTheme>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route element={<MainLayout />}>
               <Route path="/" element={<Home />} />
@@ -22,7 +22,7 @@ function App() {
 
             <Route path="*" element={<div>Route not Found!! </div>} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </GlobalTheme>
     </QueryClientProvider>
   );
